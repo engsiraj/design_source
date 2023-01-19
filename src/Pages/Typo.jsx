175ba68bcdc2924b825aa248e5a0175265269fdf
siraj <sirajbhutta50@gmail.com>
2023-01-19 13:32:49 +0500

    doc(update): typography page

---
+++ src/Pages/Typo.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+const Typo = () => {
+  return (
+      <>
+        <div className='width7'>
+        <p className="type3 bold2 padding_bottom4" >Typography 🖋</p>
+              
+        <div className="flex">
+        <a className="text_dark margin_end4">Typeface</a>
+        <a className="text_dark margin_end4">Typo</a>
+        </div>
+        <hr />
+        <p className="type4 bold1 padding_bottom2 padding_top2">TypeFace</p>      
+        <p className="text padding_bottom2">The Design Source is a User Interface Design System built in html/css and flexbox that allows resources to be reused to build consistent user interfaces.</p>
+       
+        <p className="type1 padding_bottom2">larger font size</p>
+        <p className="text padding_bottom2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum accusantium rem quaerat optio praesentium harum tenetur hic, doloribus.</p>
+        <p className="type4 bold1 padding_bottom2">poppins - font family</p>
+        <p className="text padding_bottom2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum accusantium rem quaerat optio praesentium harum tenetur hic, doloribus.</p>  
+        <p className="type4 bold1 padding_bottom2">type</p>
+              <p className="text padding_bottom2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum accusantium rem quaerat optio praesentium .</p> 
+              <div className="margin_bottom5">
+                  <div className="flex space_between margin_top4 ">
+                <p className="type1">title 01</p>
+                <p className="text ">title 01</p>
+                <p className="text text_red ">title 01</p>
+              </div> 
+               <hr />
+               <div className="flex space_between">
+                <p className="type2">title 01</p>
+                <p className="text ">title 01</p>
+                <p className="text text_red ">title 01</p>
+              </div>  
+              <hr />
+               <div className="flex space_between">
+                <p className="type3">title 01</p>
+                <p className="text ">title 01</p>
+                <p className="text text_red ">title 01</p>
+              </div>  
+
+              </div>
+               <p className="type4 margin_bottom2 bold1">text sections</p>
+              <div className="padding_start5">
+                  
+                    <p className="type4 bold1 margin_bottom1">text sections</p>
+                    <p className="text margin_bottom1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum et necessitatibus consectetur repellendus! Eius, aliquam? Amet, enim beatae adipisci alias quis at eius quisquam cupiditate facere ipsam dolorum aspernatur sunt fugiat ducimus consequatur sint esse ipsum delectus fuga iste! Minima explicabo voluptate atque dolores doloremque consequatur nobis, aut nemo vel.</p>
+              </div>
+                <p className="type4 margin_bottom2 margin_top2 bold1">Helper classes</p>
+           
+          </div>
+         
+      </>
+  )
+}
+
+export default Typo
